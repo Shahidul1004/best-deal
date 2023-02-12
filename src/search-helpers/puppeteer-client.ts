ab9ae18ab663b85b32df3ecc4964b6bec7d3c6ca
shahidul1004 <sonysust@gmail.com>
2023-02-13 02:47:57 +0600

    createBrowserFetcher

--- src/search-helpers/puppeteer-client.ts
+++ src/search-helpers/puppeteer-client.ts
@@ -9,10 +9,13 @@ let options = {};
 // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 chrome = require("chrome-aws-lambda");
 puppeteer = require("puppeteer-core");
+
+const browserFetcher = puppeteer.createBrowserFetcher();
+const revisionInfo = await browserFetcher.download('843427.');
 options = {
   args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
   defaultViewport: chrome.defaultViewport,
-  executablePath: await chrome.executablePath,
+  executablePath: await revisionInfo.executablePath,
   headless: true,
   ignoreHTTPSErrors: true,
 };
