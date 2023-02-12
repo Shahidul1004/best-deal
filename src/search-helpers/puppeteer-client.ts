b346902550ce68e4e6fe6637ff939c42c7e09f05
shahidul1004 <sonysust@gmail.com>
2023-02-13 02:21:34 +0600

    Test render

--- src/search-helpers/puppeteer-client.ts
+++ src/search-helpers/puppeteer-client.ts
@@ -2,33 +2,29 @@
 // import { Browser } from "puppeteer-core";
 // import chromium from "chrome-aws-lambda";
 
-
 let chrome = {};
 let puppeteer;
-let options = {}
+let options = {};
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-  options = {
-    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-    defaultViewport: chrome.defaultViewport,
-    executablePath: await chrome.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
-  };
-} else {
-  puppeteer = require("puppeteer");
-}
-
-
+// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+chrome = require("chrome-aws-lambda");
+puppeteer = require("puppeteer-core");
+options = {
+  args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+  defaultViewport: chrome.defaultViewport,
+  executablePath: await chrome.executablePath,
+  headless: true,
+  ignoreHTTPSErrors: true,
+};
+// } else {
+//   puppeteer = require("puppeteer");
+// }
 
 declare global {
   var browser: any;
 }
 
-export const browser =
-  global.browser || await puppeteer.launch(options);
+export const browser = global.browser || (await puppeteer.launch(options));
 if (process.env.NODE_ENV !== "production") {
   global.browser = browser;
 }
