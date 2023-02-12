775422ee25ce22bb1429ebd82f00dc15b5fb5180
shahidul1004 <sonysust@gmail.com>
2023-02-13 01:38:57 +0600

    vercel

--- src/search-helpers/puppeteer-client.ts
+++ src/search-helpers/puppeteer-client.ts
@@ -1,11 +1,34 @@
-import puppeteer, { Browser } from "puppeteer-core";
+// @ts-nocheck
+import { Browser } from "puppeteer-core";
 // import chromium from "chrome-aws-lambda";
+
+
+let chrome = {};
+let puppeteer;
+let options = {}
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+  options = {
+    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  };
+} else {
+  puppeteer = require("puppeteer");
+}
+
+
+
 declare global {
   var browser: Browser;
 }
 
 export const browser =
-  global.browser || (await puppeteer.launch({ args: ["--no-sandbox"] , executablePath: '/usr/bin/chromium-browser'}));
+  global.browser || await puppeteer.launch(options);
 if (process.env.NODE_ENV !== "production") {
   global.browser = browser;
 }
